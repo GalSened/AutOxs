4672be47c0ce7fa5ab42b9849c787b5ac0405599
Gal <galsened@gmail.com>
2021-08-23 16:59:58 +0300

    ok

--- Sanity_Suite.js
+++ Sanity_Suite.js
@@ -3,7 +3,10 @@ const somePage = require("../GitRepo/js")
 
 
 async function Sanity(){
-//await somePage.login();
+await somePage.createBuilding();
+await somePage.AddTenats();
+await somePage.AddServiceCall();
+await somePage.EditApNumber();
 await somePage.AddNewExpence();
 }
 Sanity();
