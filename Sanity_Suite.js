c8d6c2507fef91f7f5a32899c6ea419a53c1035b
Gal <galsened@gmail.com>
2021-08-24 15:35:43 +0300

    ok

--- Sanity_Suite.js
+++ Sanity_Suite.js
@@ -1,12 +1,15 @@
-
+const { Given, When, Then, After, Before } = require("@cucumber/cucumber");
+const assert = require("assert").strict;
 const somePage = require("../GitRepo/js")
 
-
-async function Sanity(){
+/*async function Sanity(){
 await somePage.createBuilding();
 await somePage.AddTenats();
 await somePage.AddServiceCall();
 await somePage.EditApNumber();
 await somePage.AddNewExpence();
 }
-Sanity();
+Sanity();*/
+
+
+Before
