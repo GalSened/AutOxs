314946125695120c76d382c87fdf51101df2962a
Gal <galsened@gmail.com>
2021-10-05 12:38:35 +0300

    arenge all functions

--- Sanity_Suite.js
+++ Sanity_Suite.js
@@ -1,6 +1,22 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var BasePage = require('C:\Users\Gal\Desktop\Oxs_Testing\AutOxs\selenium\basepage.js')
+var webdriver = require('selenium-webdriver')
 
-const assert = require("assert");
-const expect = require('chai').expect;
+class HomePage extends BasePage{
 
-const main = require('./AutOxs_Functions');
-main.login()
+
+
+    LoginPage(){
+        await driver.findElement(By.css('#view > div > div.header.headerImage > span > button.normal')).click();
+        await driver.findElement(By.xpath('//*[@id="inputs"]/input[1]')).sendKeys(MyData[1][0]);
+        await driver.findElement(By.xpath('//*[@id="inputs"]/input[2]')).sendKeys(MyData[2][0]);
+        await driver.findElement(By.xpath('//*[@id="login-win"]/form/center/input')).click();
+        await driver.manage().window().maximize();
+    }
+
+
+
+}
+
+
+module.exports = new HomePage();
